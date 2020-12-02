6a49a6dba8559770fd0fd86890e5525ad8aa07a8
Bzhamdi <bouzouitina.hamdi@gmail.com>
2020-12-02 15:09:57 -0500

    np opp

--- math/0x00-linear_algebra/12-bracin_the_elements.py
+++ math/0x00-linear_algebra/12-bracin_the_elements.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
-""" opp"""
+""" np opp"""
 
 
 def np_elementwise(mat1, mat2):
-    """ opp"""
+    """np opp"""
     add = mat1 + mat2
     diff = mat1 - mat2
     mul = mat1 * mat2
