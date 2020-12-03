82e92a07a4ec1c80e1127cb8c0f8752cd94871e3
Bzhamdi <bouzouitina.hamdi@gmail.com>
2020-12-03 18:19:18 -0500

    add matrix

---
+++ math/0x00-linear_algebra/101-the_whole_barn.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+""" add fn"""
+
+
+import numpy as np
+
+
+def add_matrices(mat1, mat2):
+    """ add fn"""
+    m1 = np.array(mat1)
+    m2 = np.array(mat2)
+    if m1.shape != m2.shape:
+        return None
+    a = np.add(m1, m2)
+    z = np.array(a)
+    return (z)
