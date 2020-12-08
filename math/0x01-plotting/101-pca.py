5bf87ebda6c198e1c22bd9ea019b31b8b322db05
Bzhamdi <bouzouitina.hamdi@gmail.com>
2020-12-07 22:12:18 -0500

    adv

---
+++ math/0x01-plotting/101-pca.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from sklearn import datasets
+from sklearn.decomposition import PCA
+
+iris = datasets.load_iris()
+X = iris.data 
+y = iris.target
+
+
+fig = plt.figure(figsize=(10, 10))
+
+ax = fig.add_subplot(111, projection='3d')
+X_reduced = PCA(n_components=3).fit_transform(iris.data)
+
+
+data_means = np.mean(data, axis=0)
+norm_data = data - data_means
+_, _, Vh = np.linalg.svd(norm_data)
+X_reduced  = np.matmul(norm_data, Vh[:3].T)
+
+ax.scatter(X_reduced[:, 0], X_reduced[:, 1], X_reduced[:,2], c=y,
+           cmap='plasma', edgecolor='face', s=100)
+ax.set_title("PCA of Iris Dataset")
+ax.set_xlabel("U1")
+ax.w_xaxis.set_ticklabels([])
+ax.set_ylabel("U2")
+ax.w_yaxis.set_ticklabels([])
+ax.set_zlabel("U3")
+ax.w_zaxis.set_ticklabels([])
+ax.set_xlim([-3,4])
+ax.set_ylim([-1.5,1])
+ax.set_zlim([-0.8,0.6])
+
+
+plt.show()
