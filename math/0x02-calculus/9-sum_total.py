504662c67e7b182e10e6747891fb2c1c44fcc86f
Bzhamdi <bouzouitina.hamdi@gmail.com>
2020-12-09 18:29:23 -0500

    cal

--- math/0x02-calculus/9-sum_total.py
+++ math/0x02-calculus/9-sum_total.py
@@ -1,3 +1,9 @@
 #!/usr/bin/env python3
+"""summation fn"""
+
+
 def summation_i_squared(n):
+    """summation fn"""
+    if n is None or n <= 0:
+        return(None)
     return (int)(n * (n + 1) * (2 * n + 1) / 6)
