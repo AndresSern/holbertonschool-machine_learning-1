d735002fd22fa288051d520eb2254e71036e94f5
Bzhamdi <bouzouitina.hamdi@gmail.com>
2020-12-10 14:36:13 -0500

    Polynomial derivativee

--- math/0x02-calculus/10-matisse.py
+++ math/0x02-calculus/10-matisse.py
@@ -8,17 +8,13 @@ def poly_derivative(poly):
     and f`(x) is [3,0,3]
     """
     p = []
-    if poly is None or not isinstance(poly, list) or poly == []:
+    if not isinstance(poly, list) or not poly:
         return None
     if not all(isinstance(n, int) for n in poly):
         return None
     if len(poly) == 1:
         return [0]
-    else:
-        poly = poly[1:]
-        for i in range(len(poly)):
-            if poly[i] < 0:
-                return None
-            else:
-                p.append(poly[i] * (i + 1))
-        return p
+    poly = poly[1:]
+    for i in range(len(poly)):
+        p.append(poly[i] * (i + 1))
+    return p
