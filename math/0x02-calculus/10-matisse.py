2065e6ba35c7da844ec3d9f3aea57e7c3c05007a
Bzhamdi <bouzouitina.hamdi@gmail.com>
2020-12-10 13:53:41 -0500

    Polynomial derivative

--- math/0x02-calculus/10-matisse.py
+++ math/0x02-calculus/10-matisse.py
@@ -10,13 +10,14 @@ def poly_derivative(poly):
     p = []
     if poly is None or not isinstance(poly, list) or not poly:
         return None
+    if len(poly) == 1:
+        if isinstance(poly[0], int):
+            return [0]
     for i in poly:
         if poly.index(i) == 1:
             p.append(poly[1])
         if poly.index(i) > 0 and poly.index(i) != 1:
             p.append(i*poly.index(i))
-    if len(poly) == 1:
-        return [0]
     test = 1
     for i in p:
         if i != 0:
