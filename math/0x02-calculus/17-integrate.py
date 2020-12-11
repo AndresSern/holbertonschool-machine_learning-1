42ca7781d1577669ba2f3eda852523ad0b4f1a10
Bzhamdi <bouzouitina.hamdi@gmail.com>
2020-12-10 19:25:52 -0500

    primitive

---
+++ math/0x02-calculus/17-integrate.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""
+that calculates the integral of a polynomial:
+
+"""
+
+
+def poly_integral(poly, C=0):
+    """
+    exp f(x) = x^3 + 3x +5 is [5, 3, 0, 1]
+    give [0, 5, 1.5, 0, 0.25]
+    """
+    p = [C]
+    if not isinstance(poly, list) or not poly:
+        return None
+    if not all(isinstance(n, int) for n in poly):
+        return None
+
+    if not isinstance(C, int):
+        return None
+    if len(poly) == 1:
+        return [0]
+    for i in range(len(poly)):
+        m = poly[i] / (i + 1)
+        if int(m) == m:
+            p.append(int(m))
+        else:
+            p.append(m)
+    return p
