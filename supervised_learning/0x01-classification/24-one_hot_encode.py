c4fb8589028303ed9371449f5d52d3bf75829024
Bzhamdi <bouzouitina.hamdi@gmail.com>
2020-12-31 01:05:57 -0500

    hot

--- supervised_learning/0x01-classification/24-one_hot_encode.py
+++ supervised_learning/0x01-classification/24-one_hot_encode.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python3
 """ one hot encode """
 
-import numpy as np
-
 
 def one_hot_encode(Y, classes):
-    if not isinstance(Y, np.ndarray)or classes < 1:
+    """
+    if not isinstance(Y, np.ndarray)or classes < 3:
         return None
     if Y.all() is None:
         return None
     encode = np.zeros((Y.size, classes))
     encode[np.arange(Y.size), Y] = 1
-    return encode.T
+    """
+    return None
