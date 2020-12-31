9f5cba77a19dfe01c2f98621cdbadabfeb9b5a40
Bzhamdi <bouzouitina.hamdi@gmail.com>
2020-12-31 16:11:50 -0500

    24

--- supervised_learning/0x01-classification/24-one_hot_encode.py
+++ supervised_learning/0x01-classification/24-one_hot_encode.py
@@ -5,17 +5,21 @@ import numpy as np
 
 
 def one_hot_encode(Y, classes):
-    '''
+    
     """ ONE HOT ENCODE"""
     if not isinstance(Y, np.ndarray)or classes < 3:
         return None
     if Y.size == 0 or classes is None:
         return None
-    encode = np.zeros((Y.size, classes))
-    encode[np.arange(Y.size), Y] = 1
-    res = encode.T
-    a, b = res.shape
-    if a != classes and b != Y.size:
+    if np.array(list(Y)).dtype == 'int64' or  np.array(list(Y)).dtype == 'int32':
+        
+        encode = np.zeros((Y.size, classes))
+        encode[np.arange(Y.size), Y] = 1
+        res = encode.T
+        a, b = res.shape
+        if a != classes and b != Y.size:
+            return None
+        
+        return res
+    else:
         return None
-    '''
-    return None
