b41f18699d15ec2c98472e9777cef5bf182afded
Bzhamdi <bouzouitina.hamdi@gmail.com>
2020-12-31 16:39:24 -0500

    25

--- supervised_learning/0x01-classification/25-one_hot_decode.py
+++ supervised_learning/0x01-classification/25-one_hot_decode.py
@@ -8,6 +8,7 @@ def one_hot_decode(one_hot):
     if not isinstance(one_hot, np.ndarray):
         return None
     lst = []
+    one_hot = one_hot.T
     for i in range(len(one_hot)):
         for j in range(len(one_hot[i])):
             if one_hot[i][j] == 1:
