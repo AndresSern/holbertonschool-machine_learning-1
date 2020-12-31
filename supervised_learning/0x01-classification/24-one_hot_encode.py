b02849b243ed9470cc917a451a4dd84d52ca3dc3
Bzhamdi <bouzouitina.hamdi@gmail.com>
2020-12-31 02:00:02 -0500

    24

--- supervised_learning/0x01-classification/24-one_hot_encode.py
+++ supervised_learning/0x01-classification/24-one_hot_encode.py
@@ -8,13 +8,9 @@ def one_hot_encode(Y, classes):
     """ ONE HOT ENCODE"""
     if not isinstance(Y, np.ndarray)or classes < 3:
         return None
-    if Y[0] is None or len(Y) < 1:
+    if len(Y) < 1:
         return None
     encode = np.zeros((Y.size, classes))
     encode[np.arange(Y.size), Y] = 1
     res = encode.T
-    a = Y.shape[0]
-    aa, bb = res.shape
-    if aa != classes or bb != a:
-        return None
     return res
