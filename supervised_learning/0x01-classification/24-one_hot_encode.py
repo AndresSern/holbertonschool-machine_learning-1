729525136fde61f6483783b78a006d8138e5c00f
Bzhamdi <bouzouitina.hamdi@gmail.com>
2021-01-01 19:31:57 -0500

    one hot encode

--- supervised_learning/0x01-classification/24-one_hot_encode.py
+++ supervised_learning/0x01-classification/24-one_hot_encode.py
@@ -5,16 +5,21 @@ import numpy as np
 
 
 def one_hot_encode(Y, classes):
-    """ ONE HOT ENCODE"""
+    """ ONE HOT ENCODE
+    Y is a numpy.ndarray containing numeric class labels
+    m is the number of examples
+    classes is the maximum number of classes found in Y
+    """
     if not isinstance(Y, np.ndarray)or classes < 3:
         return None
     if Y.size == 0 or classes is None:
         return None
-    if Y.ndim != 1:
+    try:
+        res = np.eye(classes)[Y]
+        res = res.T
+        a, b = res.shape
+        if a != classes and b != Y.size:
+            return None
+        return res
+    except Exception:
         return None
-    res = np.eye(classes)[Y]
-    res = res.T
-    a, b = res.shape
-    if a != classes and b != Y.size:
-        return None
-    return res
