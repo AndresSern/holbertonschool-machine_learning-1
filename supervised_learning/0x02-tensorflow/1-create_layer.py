9588995951de3a97fb5d4d7284e6c16ec66bee8e
Bzhamdi <bouzouitina.hamdi@gmail.com>
2021-01-06 14:54:27 -0500

    create layer

--- supervised_learning/0x02-tensorflow/1-create_layer.py
+++ supervised_learning/0x02-tensorflow/1-create_layer.py
@@ -8,7 +8,10 @@ def create_layer(prev, n, activation):
     """ create layer"""
     kernel = tf.contrib.layers.variance_scaling_initializer(factor=2.0,
                                                             mode="FAN_AVG")
-    kernel = tf.zeros_initializer()
     layer = tf.layers.dense(prev, units=n, activation=activation,
-                            kernel_initializer=kernel, name="layer")
+                            use_bias=None,
+                            kernel_initializer=kernel, name="layer",
+                            bias_regularizer=None, activity_regularizer=None,
+                            kernel_constraint=None,
+                            bias_constraint=None)
     return layer
