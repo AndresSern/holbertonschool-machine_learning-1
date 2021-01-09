96c39a80bf6e04084ae91870a7e6068079142d79
Bzhamdi <bouzouitina.hamdi@gmail.com>
2021-01-09 18:22:20 -0500

    evaluate

--- supervised_learning/0x02-tensorflow/7-evaluate.py
+++ supervised_learning/0x02-tensorflow/7-evaluate.py
@@ -7,11 +7,25 @@ import tensorflow as tf
 
 
 def evaluate(X, Y, save_path):
-    ''' evaluate'''
+    ''' evaluate FN'''
     init = tf.global_variables_initializer()
     with tf.Session() as sess:
         sess.run(init)
-        saver = tf.train.import_meta_graph('{}'.format(save_path))
+        saver = tf.train.import_meta_graph('{}.meta'.format(save_path))
         saver.restore(sess, '{}'.format(save_path))
-        x = tf.get_collection('x', scope=None)
-    print(x)
+        ax = tf.get_collection('x', scope=None)
+        ay = tf.get_collection('y', scope=None)
+        ay_pred = tf.get_collection('y_pred', scope=None)
+        aloss = tf.get_collection('loss', scope=None)
+        aaccuracy = tf.get_collection('accuracy', scope=None)
+
+        y_pred = ay_pred[0]
+        loss = aloss[0]
+        accuracy = aaccuracy[0]
+        x = ax[0]
+        y = ay[0]
+
+        y_pred = sess.run(y_pred, {x: X, y: Y})
+        accuracy = sess.run(aaccuracy, {x: X, y: Y})
+        loss = sess.run(aloss, {x: X, y: Y})
+    return(y_pred, accuracy, loss)
