9ce99c1d3fcf11d5c19ad31522aaa1674d600d4b
Bzhamdi <bouzouitina.hamdi@gmail.com>
2021-01-18 19:38:21 -0500

    f1 score

--- supervised_learning/0x04-error_analysis/4-f1_score.py
+++ supervised_learning/0x04-error_analysis/4-f1_score.py
@@ -4,6 +4,8 @@ calculates the F1 score of a confusion matrix:
 """
 
 import numpy as np
+sensitivity = __import__('1-sensitivity').sensitivity
+precision = __import__('2-precision').precision
 
 
 def f1_score(confusion):
@@ -12,8 +14,13 @@ def f1_score(confusion):
     confusion is a confusion numpy.ndarray of shape (classes, classes)
     classes is the number of classes
     """
+    '''
     FP = confusion.sum(axis=0) - np.diag(confusion)
     FN = confusion.sum(axis=1) - np.diag(confusion)
     TP = np.diag(confusion)
     F1 = TP / (TP + (0.5 * (FN + FP)))
+    '''
+    sensitivity_ = sensitivity(confusion)
+    precision_ = precision(confusion)
+    F1 = 2 * precision_ * sensitivity_ / (precision_ + sensitivity_)
     return F1
