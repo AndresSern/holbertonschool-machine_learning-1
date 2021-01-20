deba46cbc9263c91890f675d105c690f783268fb
Bzhamdi <bouzouitina.hamdi@gmail.com>
2021-01-20 16:20:35 -0500

    l2 ts

--- supervised_learning/0x05-regularization/2-l2_reg_cost.py
+++ supervised_learning/0x05-regularization/2-l2_reg_cost.py
@@ -14,5 +14,7 @@ def l2_reg_cost(cost):
         *Returns: a tensor containing the cost of
         the network accounting for L2 regularization
     '''
-    cost = cost + tf.losses.get_regularization_loss()
-    return cost
+    reg_losses = tf.get_collection(tf.GraphKeys.REGULARIZATION_LOSSES)
+    reg_constant = 0.01
+    cst = cost + reg_constant * sum(reg_losses)
+    return cst
