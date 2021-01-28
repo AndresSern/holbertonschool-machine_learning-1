7036947193a17df084b5744b0336d393eb6cc38a
Bzhamdi <bouzouitina.hamdi@gmail.com>
2021-01-28 11:41:31 -0500

    convolve

--- math/0x04-convolutions_and_pooling/0-convolve_grayscale_valid.py
+++ math/0x04-convolutions_and_pooling/0-convolve_grayscale_valid.py
@@ -1,15 +1,31 @@
 #!/usr/bin/env python3
-"""performs a valid convolution on grayscale images"""
+"""
+performs a valid convolution on grayscale images
+"""
 import numpy as np
 
 
 def convolve_grayscale_valid(images, kernel):
-    """performs a valid convolution on grayscale images"""
+    """
+    images is a numpy.ndarray with shape (m, h, w)
+    containing multiple grayscale images
+        *m is the number of images
+        *h is the height in pixels of the images
+        *w is the width in pixels of the images
+    kernel is a numpy.ndarray with shape (kh, kw)
+    containing the kernel for the convolution
+        *kh is the height of the kernel
+        *kw is the width of the kernel
+    Returns: a numpy.ndarray containing the convolved images
+    """
     m, h, w = images.shape
     kh, kw = kernel.shape
-    out_images = np.zeros((m, h - kh + 1, w - kw + 1))
-    for i in range(h - kh + 1):
-        for j in range(w - kw + 1):
-            out_images[:, i, j] = np.sum(
-                kernel * images[:, i: i + kh, j: j + kw], axis=(1, 2))
-    return out_images
+
+    output_h = h - kh + 1
+    output_w = w - kw + 1
+    output = np.zeros((m, output_h, output_w))
+    for x in range(output_w):
+        for y in range(output_h):
+            output[:, y, x] = (kernel * images[:, y: y + kh,
+                                               x: x + kw]).sum(axis=(1, 2))
+    return output
