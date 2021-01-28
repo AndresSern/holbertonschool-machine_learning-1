1e65cf957a123d98fd12f87be6ee0548257c84fd
Bzhamdi <bouzouitina.hamdi@gmail.com>
2021-01-28 11:30:34 -0500

    fuck

--- math/0x04-convolutions_and_pooling/0-convolve_grayscale_valid.py
+++ math/0x04-convolutions_and_pooling/0-convolve_grayscale_valid.py
@@ -9,15 +9,6 @@ from math import ceil, floor
 def convolve_grayscale_valid(images, kernel):
     """
     images is a numpy.ndarray with shape (m, h, w)
-    containing multiple grayscale images
-        *m is the number of images
-        *h is the height in pixels of the images
-        *w is the width in pixels of the images
-    kernel is a numpy.ndarray with shape (kh, kw)
-    containing the kernel for the convolution
-        *kh is the height of the kernel
-        *kw is the width of the kernel
-    Returns: a numpy.ndarray containing the convolved images
     """
     m, h, w = images.shape
     kh, kw = kernel.shape
