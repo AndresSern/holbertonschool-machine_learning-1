acd0eb00809f6fd6c1cbece8434e65366e7539b4
Bzhamdi <bouzouitina.hamdi@gmail.com>
2021-02-11 20:31:25 -0500

    resnet50

--- supervised_learning/0x08-deep_cnns/4-resnet50.py
+++ supervised_learning/0x08-deep_cnns/4-resnet50.py
@@ -20,29 +20,29 @@ def resnet50():
     """ 7x7 ,64, stride = 2"""
     X = K.layers.Conv2D(64, (7, 7), strides=(2, 2),
                         kernel_initializer=kernel)(X_input)
-    X = K.layers.BatchNormalization(axis=3)(X)
+    X = K.layers.BatchNormalization()(X)
     X = K.layers.Activation('relu')(X)
     """ 3x3 maxpool stride = 2"""
     X = K.layers.MaxPooling2D((3, 3), strides=(2, 2), padding='same')(X)
     """ conv2_x * 3"""
-    X = projection_block(X, filters=[64, 64, 256], s=1)
+    X = projection_block(X, [64, 64, 256], s=1)
     X = identity_block(X, [64, 64, 256])
     X = identity_block(X, [64, 64, 256])
 
     """ conv2_x * 4"""
-    X = projection_block(X, filters=[128, 128, 512])
+    X = projection_block(X, [128, 128, 512])
     X = identity_block(X, [128, 128, 512])
     X = identity_block(X, [128, 128, 512])
     X = identity_block(X, [128, 128, 512])
     """ conv2_x * 6"""
-    X = projection_block(X, filters=[256, 256, 1024])
+    X = projection_block(X, [256, 256, 1024])
     X = identity_block(X, [256, 256, 1024])
     X = identity_block(X,  [256, 256, 1024])
     X = identity_block(X,  [256, 256, 1024])
     X = identity_block(X,  [256, 256, 1024])
     X = identity_block(X,  [256, 256, 1024])
     """ conv2_x * 3"""
-    X = projection_block(X, filters=[512, 512, 2048])
+    X = projection_block(X, [512, 512, 2048])
     X = identity_block(X,  [512, 512, 2048])
     X = identity_block(X,  [512, 512, 2048])
     """ AveragePooling2D '1000-d fc'"""
