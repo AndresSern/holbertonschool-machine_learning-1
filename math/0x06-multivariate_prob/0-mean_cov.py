78b9816fe31afda863b1fcc0d214f77747e2efef
Bzhamdi <bouzouitina.hamdi@gmail.com>
2021-03-22 19:16:33 -0400

    test

--- math/0x06-multivariate_prob/0-mean_cov.py
+++ math/0x06-multivariate_prob/0-mean_cov.py
@@ -6,18 +6,14 @@ calculates the mean and covariance of a data set
 
 
 def mean_cov(X):
-    """
-    Args:
-    X is a numpy.ndarray of shape (n, d) containing the data set
-    Return:
-        he mean and covariance of a data set
-    """
-
-    if not isinstance(X, np.ndarray) or len(X.shape) != 2:
+    """ mean and covariance"""
+    if not isinstance(X, np.ndarray):
+        raise TypeError("X must be a 2D numpy.ndarray")
+    if len(X.shape) != 2:
         raise TypeError("X must be a 2D numpy.ndarray")
     if X.shape[0] < 2:
         raise ValueError("X must contain multiple data points")
     n, d = X.shape
-    mean = np.mean(X, axis=0)
-    a = X - mean
-    return np.mean(X, axis=0), np.matmul(a.T, a) / (n - 1)
+    mean = np.mean(X, axis=0, keepdims=True)
+    conv = np.matmul((X - mean).T, (X - mean)) / (n - 1)
+    return mean, conv
