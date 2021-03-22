297e611aed18d1512d8b7fca03c01a01b762d890
Bzhamdi <bouzouitina.hamdi@gmail.com>
2021-03-22 19:11:50 -0400

    mean_conv

--- math/0x06-multivariate_prob/0-mean_cov.py
+++ math/0x06-multivariate_prob/0-mean_cov.py
@@ -9,10 +9,6 @@ def mean_cov(X):
     """
     Args:
     X is a numpy.ndarray of shape (n, d) containing the data set
-    If X is not a 2D numpy.ndarray, raise a TypeError with the message
-        X must be a 2D numpy.ndarray
-    If n is less than 2, raise a ValueError with the message X must
-         contain multiple data points
     Return:
         he mean and covariance of a data set
     """
@@ -23,4 +19,5 @@ def mean_cov(X):
         raise ValueError("X must contain multiple data points")
     n, d = X.shape
     mean = np.mean(X, axis=0)
-    return np.mean(X, axis=0), np.matmul((X - mean).T, X - mean) / (n - 1)
+    a = X - mean
+    return np.mean(X, axis=0), np.matmul(a.T, a) / (n - 1)
