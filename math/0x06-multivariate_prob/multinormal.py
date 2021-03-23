522047127e48b39b3c7c8db217db1ff255e3f0d2
Bzhamdi <bouzouitina.hamdi@gmail.com>
2021-03-22 22:50:06 -0400

    multinormal

--- math/0x06-multivariate_prob/multinormal.py
+++ math/0x06-multivariate_prob/multinormal.py
@@ -6,6 +6,9 @@ import numpy as np
 
 
 class MultiNormal:
+    """
+    multiNormal
+    """
     def __init__(self, data):
         """
         Multivariate Normal distribution
@@ -14,8 +17,8 @@ class MultiNormal:
         if not isinstance(data, np.ndarray):
             raise TypeError("data must be a 2D numpy.ndarray")
         if len(data.shape) != 2:
-            raise TypeError("X must be a 2D numpy.ndarray")
-        if data.shape[0] < 2:
+            raise TypeError("data must be a 2D numpy.ndarray")
+        if data.shape[1] < 2:
             raise ValueError("data must contain multiple data points")
         d, n = data.shape
         self.mean = np.mean(data, axis=1, keepdims=True)
