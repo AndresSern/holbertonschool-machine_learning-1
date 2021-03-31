cb1f2c29e0adec10b8cdc32080f683b7265e34e2
Bzhamdi <bouzouitina.hamdi@gmail.com>
2021-03-31 18:43:19 -0400

    variance

--- unsupervised_learning/0x01-clustering/2-variance.py
+++ unsupervised_learning/0x01-clustering/2-variance.py
@@ -15,10 +15,6 @@ def variance(X, C):
     Returns: var, or None on failure
         -var is the total variance
     """
-    if type(X) is not np.ndarray or len(X.shape) != 2:
-        return None
-    if type(C) is not np.ndarray or len(C.shape) != 2:
-        return None
     if (not isinstance(X, np.ndarray) or not isinstance(C, np.ndarray)
             or len(X.shape) != 2 or len(C.shape) != 2
             or X.shape[1] != C.shape[1]):
