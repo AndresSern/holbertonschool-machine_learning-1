7aded78fc70410d10bd72abbcc1644c3c3ed0f96
Bzhamdi <bouzouitina.hamdi@gmail.com>
2021-04-01 17:29:15 -0400

    pdf

---
+++ unsupervised_learning/0x01-clustering/5-pdf.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+"""calculates the probability density function of a Gaussian distribution"""
+import numpy as np
+
+
+def pdf(X, m, S):
+    """
+    ARGS:
+    -X is a numpy.ndarray of shape (n, d) containing the data points whose
+
+    -m is a numpy.ndarray of shape (d,) containing the mean of the distribution
+    -S is a numpy.ndarray of shape (d, d) containing the covariance of the
+    distribution
+
+    Returns: P, or None on failure
+    P is a numpy.ndarray of shape (n,) containing the PDF values for each
+    data point
+    """
+    if type(X) is not np.ndarray or len(X.shape) != 2:
+        return None
+    if type(m) is not np.ndarray or len(m.shape) != 1:
+        return None
+    if type(S) is not np.ndarray or len(S.shape) != 2:
+        return None
+    if X.shape[1] != m.shape[0] or X.shape[1] != S.shape[0]:
+        return None
+    if S.shape[1] != S.shape[0]:
+        return None
+    m1 = X - m
+    inv_S = np.linalg.inv(S)
+    mul1 = np.matmul(m1, inv_S)
+    var1 = np.exp(np.matmul(mul1, m1.T).diagonal() * -0.5)
+    var2 = (((2 * np.pi) ** X.shape[1]) * np.linalg.det(S)) ** (0.5)
+    pdf = var1/var2
+    return np.where(pdf < 1e-300, 1e-300, pdf)
--- unsupervised_learning/0x01-clustering/README.md
+++ unsupervised_learning/0x01-clustering/README.md
@@ -1,1 +1,8 @@
-read
+ # 0x01. Clustering
+Kmeans algorithm is an iterative algorithm that tries to partition the dataset into Kpre-defined distinct non-overlapping subgroups (clusters) where each data point belongs to only one group. It tries to make the intra-cluster data points as similar as possible while also keeping the clusters as different (far) as possible. It assigns data points to a cluster such that the sum of the squared distance between the data points and the cluster’s centroid (arithmetic mean of all the data points that belong to that cluster) is at the minimum. The less variation we have within clusters, the more homogeneous (similar) the data points are within the same cluster.
+## 2. Variance
+
+![alt text](https://mrmint.fr/wp-content/ql-cache/quicklatex.com-36d867f79592e46e6e0556a1ce094da9_l3.svg)
+## 5. PDF
+calculates the probability density function of a Gaussian distribution
+![alt text](https://i.ibb.co/dBKy38y/download.png)
