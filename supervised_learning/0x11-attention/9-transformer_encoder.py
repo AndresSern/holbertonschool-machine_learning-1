290457d5147092f83bda6218d1d6eea560474af9
Bzhamdi <bouzouitina.hamdi@gmail.com>
2021-05-21 18:49:20 -0400

    Transformer Encoder

--- supervised_learning/0x11-attention/9-transformer_encoder.py
+++ supervised_learning/0x11-attention/9-transformer_encoder.py
@@ -44,10 +44,12 @@ class Encoder(tf.keras.layers.Layer):
         Returns: a tensor of shape (batch, input_seq_len, dm)
             containing the encoder output
         """
-
+        seq_len = x.shape[1]
+        ''' or
         seq_len = tf.shape(x)[1]
         with tf.Session() as sess:
             seq_len = sess.run(seq_len)
+        '''
 
         # adding embedding and position encoding.
         x = self.embedding(x)  # (batch_size, input_seq_len, d_model)
