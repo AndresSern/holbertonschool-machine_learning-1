2952f81ae954713f4cae84e7019457231e3cee95
Bzhamdi <bouzouitina.hamdi@gmail.com>
2021-05-21 19:37:29 -0400

    transformer encoder

--- supervised_learning/0x11-attention/9-transformer_encoder.py
+++ supervised_learning/0x11-attention/9-transformer_encoder.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 """ Transformer Encoder"""
+
 import tensorflow as tf
 EncoderBlock = __import__('7-transformer_encoder_block').EncoderBlock
 positional_encoding = __import__('4-positional_encoding').positional_encoding
@@ -18,14 +19,14 @@ class Encoder(tf.keras.layers.Layer):
             -max_seq_len - the maximum sequence length possible
             -drop_rate - the dropout rate
         """
-        super().__init__()
-        self.N = N
+        super(Encoder, self).__init__()
+
         self.dm = dm
+        self.N = N
         self.embedding = tf.keras.layers.Embedding(input_vocab, dm)
-        self.positional_encoding = positional_encoding(max_seq_len, self.dm)
+        self.positional_encoding = positional_encoding(max_seq_len, dm)
         self.blocks = [EncoderBlock(dm, h, hidden, drop_rate)
                        for _ in range(N)]
-
         self.dropout = tf.keras.layers.Dropout(drop_rate)
 
     def call(self, x, training, mask):
@@ -40,7 +41,6 @@ class Encoder(tf.keras.layers.Layer):
         Returns: a tensor of shape (batch, input_seq_len, dm)
             containing the encoder output
         """
-       
         seq_len = x.shape[1]
 
         # adding embedding and position encoding.
