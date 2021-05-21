81c483230e514b5bcddbfd93203c5448686a8092
Bzhamdi <bouzouitina.hamdi@gmail.com>
2021-05-21 11:26:30 -0400

    transformer encoder block

--- supervised_learning/0x11-attention/7-transformer_encoder_block.py
+++ supervised_learning/0x11-attention/7-transformer_encoder_block.py
@@ -47,7 +47,7 @@ class EncoderBlock(tf.keras.layers.Layer):
         attn_output = self.dropout1(attn_output, training=training)
         out1 = self.layernorm1(x + attn_output)
 
-        ffn_output = self.ffn(out1)
+        ffn_output = out1
         ffn_output = self.dropout2(ffn_output, training=training)
         out2 = self.layernorm2(out1 + ffn_output)
 
