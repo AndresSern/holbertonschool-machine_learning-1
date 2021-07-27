cfe93dc7548a1f2d9c660e0943449ed605fe25e3
Bzhamdi <bouzouitina.hamdi@gmail.com>
2021-07-27 00:26:24 -0400

    faster

--- pipeline/0x01-apis/4-rocket_frequency.py
+++ pipeline/0x01-apis/4-rocket_frequency.py
@@ -16,19 +16,30 @@ if __name__ == '__main__':
     dct_nbr_rocket = {i: 0 for i in name_set}
     '''
     dct_nbr_rocket = {}
-    #  ---------------
+    #  --------get all launches
     url = "https://api.spacexdata.com/v4/launches/"
     req1 = requests.get(url)
     data = req1.json()
-    for item in data:
+    # get all roks
+    roks = requests.get("https://api.spacexdata.com/v4/rockets/").json()
+    roks_id = set()
+    for i in roks:
+        roks_id.add(i['id'])
+    b = {}
+    for i in roks_id:
         rok = requests.get("https://api.spacexdata.com/v4/rockets/" +
-                           item['rocket']).json()
+                           i).json()
+        b[i] = rok['name']
 
-        if rok['name'] in dct_nbr_rocket.keys():
-            dct_nbr_rocket[rok['name']] += 1
+    for item in data:
+        # rok = requests.get("https://api.spacexdata.com/v4/rockets/" +
+        #                   item['rocket']).json()
+        # print(b[item['rocket']])
+
+        if b[item['rocket']] in dct_nbr_rocket:
+            dct_nbr_rocket[b[item['rocket']]] += 1
         else:
-            dct_nbr_rocket[rok['name']] = 1
-
+            dct_nbr_rocket[b[item['rocket']]] = 1
     result = {k: v for k, v in sorted(dct_nbr_rocket.items(),
                                       key=lambda item: item[1], reverse=True)}
     for key, value in result.items():
