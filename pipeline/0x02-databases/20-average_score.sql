0778b3dadb24a4bad727f5f1205e2a8c614f6da7
Bzhamdi <bouzouitina.hamdi@gmail.com>
2021-08-02 13:35:28 -0400

    procedure

--- pipeline/0x02-databases/20-average_score.sql
+++ pipeline/0x02-databases/20-average_score.sql
@@ -8,8 +8,8 @@ CREATE  PROCEDURE ComputeAverageScoreForUser (
 BEGIN
 
 
-SELECT AVG(score) INTO @avg FROM corrections WHERE user_id = user_id;
+SELECT AVG(score) INTO @avg FROM corrections WHERE corrections.user_id = user_id;
 UPDATE users SET average_score = @avg WHERE id = user_id;
 END$$
 
-DELIMITER ; 
+DELIMITER ;
