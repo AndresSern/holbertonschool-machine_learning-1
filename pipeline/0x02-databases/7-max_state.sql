ea5164649eaee07e56c2c12cd5183f82e60e88f5
Bzhamdi <bouzouitina.hamdi@gmail.com>
2021-08-01 21:51:04 -0400

    sql

--- pipeline/0x02-databases/7-max_state.sql
+++ pipeline/0x02-databases/7-max_state.sql
@@ -1,2 +1,2 @@
 -- displays the max temperature of each state (ordered by State name).
-SELECT state, MAX(value) as max_temp from temperatures GROUP By state ORDER by state;
+SELECT state, MAX(value) As max_temp FROM temperatures GROUP By state ORDER by state;
