decfa231257cfbf3e54e3281ccdf6a723eba1eab
Bzhamdi <bouzouitina.hamdi@gmail.com>
2021-08-03 00:08:17 -0400

    tet

--- pipeline/0x02-databases/103-average_weighted_score.sql
+++ pipeline/0x02-databases/103-average_weighted_score.sql
@@ -1,13 +1,13 @@
 --  script that creates a stored procedure ComputeAverageWeightedScoreForUser that computes and store
 -- the average weighted score for a student.
--- DROP PROCEDURE ComputeAverageWeightedScoreForUser;
+DROP PROCEDURE ComputeAverageWeightedScoreForUser;
 DELIMITER $$
 CREATE  PROCEDURE ComputeAverageWeightedScoreForUser(
-	IN user_id INT)
+	IN user_idd INTEGER)
 	BEGIN
 		UPDATE users SET average_score=(
-			SELECT SUM(score*weight)/SUM(weight) FROM corrections co
+			SELECT SUM(co.score*pr.weight)/SUM(pr.weight) FROM corrections co
 				JOIN projects  pr ON co.project_id=pr.id
-				WHERE pr.user_id=user_id) WHERE id=user_id;
+				WHERE co.user_id=user_idd) WHERE id=user_idd;
 	END $$
 DELIMITER ;
